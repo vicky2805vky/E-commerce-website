5959ca6f12dc5f5d01a98998428fe1028d373b51
vicky2805vky <vicky2805vky@gmail.com>
2024-10-19 12:26:00 +0530

    moved router to seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-
 import { auth } from "configs/firebase";
 
 import Header from "features/header/Header";
-import ProductPage from "features/product/ProductPage";
-import Cart from "features/cart/Cart";
 import ErrorPage from "features/error/ErrorPage";
-import Product from "features/product/components/productBuy/Product";
-import Account from "features/auth/Account";
 import RedirectPage from "features/redirect/RedirectPage";
 import LoadingScreen from "components/LoadingScreen";
 import ToastNotification from "components/ToastNotification";
@@ -15,15 +9,10 @@ import ToastNotification from "components/ToastNotification";
 import useReduxData from "hooks/useReduxData";
 import useInitiateApp from "hooks/useInitiateApp";
 import useAuthState from "hooks/useAuthState";
-import DeleteAccount from "features/auth/components/profile/DeleteAccount";
-import DeleteConfirm from "features/auth/components/profile/DeleteConfirm";
-import Admin from "features/admin/Admin";
-import AddProducts from "features/admin/components/addProducts/AddProducts";
-import ProductManager from "features/admin/components/viewProducts/ProductManager";
-import EditProducts from "features/admin/components/addProducts/EditProducts";
+import AppRouter from "components/AppRouter";
 
 function App() {
-  const { products, error, isLoggedIn } = useReduxData();
+  const { products, error } = useReduxData();
   useAuthState();
 
   useInitiateApp(auth.currentUser);
@@ -47,23 +36,7 @@ function App() {
       <div className="App">
         <Header />
         <div style={{ width: "100%", height: "calc(85vh - 51px)" }}>
-          <Routes>
-            <Route path="/" element={<ProductPage />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Account />} />
-            <Route path="/admin" element={<Admin />}>
-              <Route path="/admin/products" element={<ProductManager />} />
-              <Route
-                path="/admin/products/:id/edit"
-                element={<EditProducts />}
-              />
-              <Route path="/admin/products/add" element={<AddProducts />} />
-            </Route>
-            {isLoggedIn && <Route path="/delete" element={<DeleteAccount />} />}
-            <Route path="/delete-confirmation" element={<DeleteConfirm />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
+          <AppRouter />
           <ToastNotification />
         </div>
       </div>
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,36 @@
+import Admin from "features/admin/Admin";
+import AddProducts from "features/admin/components/addProducts/AddProducts";
+import EditProducts from "features/admin/components/addProducts/EditProducts";
+import ProductManager from "features/admin/components/viewProducts/ProductManager";
+import Account from "features/auth/Account";
+import DeleteAccount from "features/auth/components/profile/DeleteAccount";
+import DeleteConfirm from "features/auth/components/profile/DeleteConfirm";
+import Cart from "features/cart/Cart";
+import ErrorPage from "features/error/ErrorPage";
+import Product from "features/product/components/productBuy/Product";
+import ProductPage from "features/product/ProductPage";
+import useReduxData from "hooks/useReduxData";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const AppRouter = () => {
+  const { isLoggedIn } = useReduxData();
+  return (
+    <Routes>
+      <Route path="/" element={<ProductPage />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/profile" element={<Account />} />
+      <Route path="/admin" element={<Admin />}>
+        <Route path="/admin/products" element={<ProductManager />} />
+        <Route path="/admin/products/:id/edit" element={<EditProducts />} />
+        <Route path="/admin/products/add" element={<AddProducts />} />
+      </Route>
+      {isLoggedIn && <Route path="/delete" element={<DeleteAccount />} />}
+      <Route path="/delete-confirmation" element={<DeleteConfirm />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
